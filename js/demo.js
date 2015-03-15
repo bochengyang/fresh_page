3f07f997b47d093dd0a35eea600357b1299ac2cc
Bocheng Yang <bocheng.yang@gmail.com>
2015-03-15 20:32:24 +0800

    Adding how we meet picture and gallery lightbox oeration

--- js/demo.js
+++ js/demo.js
@@ -46,11 +46,13 @@ $(function () {
         });
     });
 
-    $('#borderless-checkbox').on('change', function () {
+    $('#blueimp-gallery').data('useBootstrapModal', false);
+    $('#blueimp-gallery').toggleClass('blueimp-gallery-controls', true);
+    /*$('#borderless-checkbox').on('change', function () {
         var borderless = $(this).is(':checked');
         $('#blueimp-gallery').data('useBootstrapModal', !borderless);
         $('#blueimp-gallery').toggleClass('blueimp-gallery-controls', borderless);
-    });
+    });*/
 
     $('#fullscreen-checkbox').on('change', function () {
         $('#blueimp-gallery').data('fullScreen', $(this).is(':checked'));
